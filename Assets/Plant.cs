3f1f7d5de90e93bf86c2b4888595c517447b7be9
Böbi <148640182+Bobiyi@users.noreply.github.com>
2026-01-11 22:26:02 +0100

    Plant

---
+++ Assets/Plant.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Plant : MonoBehaviour
+{
+    private int health;
+    private int cost;
+
+    protected Plant(int health, int cost)
+    {
+        this.Health = health;
+        this.Cost = cost;
+    }
+
+    public int Health { get => health; protected set => health = value; }
+    public int Cost { get => cost; protected set => cost = value; }
+}
